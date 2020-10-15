3d700f721849fbd7df3ebb6955b3e24bf4359a55
TuncayBasak25 <tuncay.basak@outlook.com>
2020-10-15 14:47:19 +0200

    S

--- middleware/autoLogger.js
+++ middleware/autoLogger.js
@@ -16,6 +16,10 @@ const autoLogger = async (req, res, next) => {
     let user = await secret.getUser();
     req.user = user;
   }
+  else
+  {
+    req.user = await db.User.findOne({ where: { id: 1 } });
+  }
 
   next();
 }
