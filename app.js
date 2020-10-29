73fee9ace53e810f1af5597574d879dbb94399ef
Tuncay Basak <tuncay.basak@outlook.com>
2020-10-29 15:59:42 +0100

    s

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ var app = express();
 
 var bodyParser = require('body-parser');
 app.use(bodyParser.json({limit: '5mb'}));
-
+//
 
 app.use(express.static(path.join(__dirname, 'public')));
 //SESSIONS
