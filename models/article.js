87ad341c5183a6d82fc529bc5f9c8805f525133d
Tuncay Basak <tuncay.basak@outlook.com>
2020-10-29 09:29:45 +0100

    S

--- models/article.js
+++ models/article.js
@@ -3,7 +3,7 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Article extends Model {
+  class Article extends Model {//
     /**
      * Helper method for defining associations.
      * This method is not a part of Sequelize lifecycle.
